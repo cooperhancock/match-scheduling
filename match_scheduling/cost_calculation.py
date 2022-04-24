faeeed8bb3bf018fcfe297d44016a0e2175d4cff
Ritik Mishra <ritik.mishra314@gmail.com>
2022-04-24 15:58:38 -0500

    use partial application to avoid special casing it

--- match_scheduling/cost_calculation.py
+++ match_scheduling/cost_calculation.py
@@ -1,11 +1,14 @@
+from functools import partial
+from typing import Callable, List
 from match_scheduling.constraints import alliance_station_evenness, has_correct_num_matches, num_repeated_allied_team, num_repeated_opposing_team, red_vs_blue_matches, spaces_between_matches
-from match_scheduling.match_schedule import MatchSchedule
+from match_scheduling.match_schedule import Match, MatchSchedule
 
 # ideal num of matches per team
 IDEAL_NUM_MATCHES = 6
 
 # constraint funcs that use team and match list arguments
-TEAM_MATCH_FUNCS = [
+TEAM_MATCH_FUNCS: List[Callable[[int, List[Match]], float]] = [
+    partial(has_correct_num_matches, IDEAL_NUM_MATCHES),
     spaces_between_matches,
     num_repeated_allied_team,
     num_repeated_opposing_team,
@@ -20,9 +23,7 @@ WEIGHTS = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 def cost(schedule: MatchSchedule) -> float:
     total_cost = 0.0
     for team in schedule.teams:
-        correct_num_matches = has_correct_num_matches(IDEAL_NUM_MATCHES, team, schedule.matches)
         cost_list = [x(team, schedule.matches) for x in TEAM_MATCH_FUNCS]
-        cost_list.insert(0, correct_num_matches)
         total_cost += sum(x*y for x, y in zip(cost_list, WEIGHTS))
     return total_cost
 
