ef80f1f51c0088916661f4d6db4c0f6b875c3aa6
cooperhancock <cooperhancock18@gmail.com>
2022-04-25 01:14:52 -0500

    added spacing print out

--- match_scheduling/__main__.py
+++ match_scheduling/__main__.py
@@ -1,3 +1,4 @@
+from match_scheduling.constraints import spaces_between_matches
 from match_scheduling.simulated_annealing import simulated_annealing
 
 teams = list(range(36))
@@ -5,3 +6,5 @@ num_matches = 6
 
 out_schedule = simulated_annealing(teams, num_matches)
 print(out_schedule.format_pretty())
+for team in out_schedule.teams:
+            print('team', team, ':', spaces_between_matches(team, out_schedule.matches))
